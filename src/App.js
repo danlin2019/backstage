2c14d075f6643fd90d89c03f1f5446c96dc77cec
wcc723 <agito723@gmail.com>
2023-01-29 09:25:07 +0800

    Dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Routes, Route, Link } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Link to="/login">登入</Link>
+      <Link to="/login">登入</Link> | 
+      <Link to="/dashboard">Dashboard</Link>
       <Routes>
         <Route path="/login" element={<Login/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
       </Routes>
     </div>
   );
