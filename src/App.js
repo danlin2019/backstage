4a7a0617bb4ed2f810aad84b4e06adb0d06e215f
wcc723 <agito723@gmail.com>
2023-02-14 10:31:40 +0800

    deleteModal

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Modals from './pages/Modals';
 import ProductModal from './pages/ProductModal';
+import DeleteModal from './pages/DeleteModal';
 
 function App() {
   return (
-    <div className="App">
-      <Link to="/login">登入</Link> | 
-      <Link to="/dashboard">Dashboard</Link> |
-      <Link to="/modal/product">Product Modal</Link>
-      
+    <div className='App'>
+      <Link to='/login'>登入</Link> |<Link to='/dashboard'>Dashboard</Link> |
+      <Link to='/modal/product'>Product Modal</Link> | 
+      <Link to='/modal/delete'>Delete Modal</Link>
       <Routes>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/dashboard" element={<Dashboard/>}></Route>
-        <Route path="/modal" element={<Modals/> }>
-          <Route path='product' element={<ProductModal/>}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/modal' element={<Modals />}>
+          <Route path='product' element={<ProductModal />}></Route>
+          <Route path='delete' element={<DeleteModal />}></Route>
         </Route>
       </Routes>
     </div>
