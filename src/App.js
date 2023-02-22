e327fac3813a4298b0cda640652ecef822d3d263
wcc723 <agito723@gmail.com>
2023-02-22 14:54:39 +0800

    增加優惠券

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import Login from './pages/Login';
 import Modals from './pages/Modals';
 import ProductModal from './pages/ProductModal';
 import DeleteModal from './pages/DeleteModal';
+import CouponModal from './pages/CouponModal';
 
 function App() {
   return (
     <div className='App'>
       <Link to='/login'>登入</Link> |<Link to='/dashboard'>Dashboard</Link> |
-      <Link to='/modal/product'>Product Modal</Link> | 
-      <Link to='/modal/delete'>Delete Modal</Link>
+      <Link to='/modal/product'>Product Modal</Link> |
+      <Link to='/modal/delete'>Delete Modal</Link> |
+      <Link to='/modal/coupon'>Coupon Modal</Link> |
       <Routes>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/dashboard' element={<Dashboard />}></Route>
         <Route path='/modal' element={<Modals />}>
           <Route path='product' element={<ProductModal />}></Route>
           <Route path='delete' element={<DeleteModal />}></Route>
+          <Route path='coupon' element={<CouponModal />}></Route>
         </Route>
       </Routes>
     </div>
---
+++ src/pages/CouponModal.js
@@ -0,0 +1,93 @@
+function CouponModal() {
+  return (
+    <div
+      className='modal fade'
+      id='couponModal'
+      tabIndex='-1'
+      aria-labelledby='exampleModalLabel'
+      aria-hidden='true'
+    >
+      <div className='modal-dialog modal-lg'>
+        <div className='modal-content'>
+          <div className='modal-header'>
+            <h1 className='modal-title fs-5' id='exampleModalLabel'>
+              建立新優惠券
+            </h1>
+            <button type='button' className='btn-close' aria-label='Close' />
+          </div>
+          <div className='modal-body'>
+            <div className='mb-2'>
+              <label className='w-100' htmlFor='title'>
+                標題
+                <input
+                  type='text'
+                  id='title'
+                  placeholder='請輸入標題'
+                  name='title'
+                  className='form-control mt-1'
+                />
+              </label>
+            </div>
+            <div className='row'>
+              <div className='col-md-6 mb-2'>
+                <label className='w-100' htmlFor='percent'>
+                  折扣（%）
+                  <input
+                    type='text'
+                    name='percent'
+                    id='percent'
+                    placeholder='請輸入折扣（%）'
+                    className='form-control mt-1'
+                  />
+                </label>
+              </div>
+              <div className='col-md-6 mb-2'>
+                <label className='w-100' htmlFor='due_date'>
+                  到期日
+                  <input
+                    type='date'
+                    id='due_date'
+                    name='due_date'
+                    placeholder='請輸入到期日'
+                    className='form-control mt-1'
+                  />
+                </label>
+              </div>
+              <div className='col-md-6 mb-2'>
+                <label className='w-100' htmlFor='code'>
+                  優惠碼
+                  <input
+                    type='text'
+                    id='code'
+                    name='code'
+                    placeholder='請輸入優惠碼'
+                    className='form-control mt-1'
+                  />
+                </label>
+              </div>
+            </div>
+            <label className='form-check-label' htmlFor='is_enabled'>
+              <input
+                className='form-check-input me-2'
+                type='checkbox'
+                id='is_enabled'
+                name='is_enabled'
+              />
+              是否啟用
+            </label>
+          </div>
+          <div className='modal-footer'>
+            <button type='button' className='btn btn-secondary'>
+              關閉
+            </button>
+            <button type='button' className='btn btn-primary'>
+              儲存
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CouponModal;
